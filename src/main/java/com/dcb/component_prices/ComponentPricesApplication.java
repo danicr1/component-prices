458820829116ec9bb5780ee4e59c9b27b60ef846
danicr1 <danicri96@hotmail.com>
2025-04-09 23:38:09 +0200

    Initial commit

---
+++ src/main/java/com/dcb/component_prices/ComponentPricesApplication.java
@@ -0,0 +1,13 @@
+package com.dcb.component_prices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponentPricesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComponentPricesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dcb/component_prices/ComponentPricesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcb.component_prices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponentPricesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
